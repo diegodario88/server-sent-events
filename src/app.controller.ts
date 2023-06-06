cc9c5f180242f302a587c51b11a543d5472ca257
Diego Dario <25825145+diegodario88@users.noreply.github.com>
2023-06-05 23:08:50 -0300

    feat: add basic sse

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get, Logger, Res } from '@nestjs/common';
+import { Controller, Get, MessageEvent, Res, Sse } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Response } from 'express';
 import { readFileSync } from 'fs';
 import { join } from 'path';
 import { cwd } from 'process';
+import { Observable } from 'rxjs';
 
 @Controller()
 export class AppController {
@@ -13,5 +14,10 @@ export class AppController {
   index(@Res() response: Response) {
     const index = readFileSync(join(cwd(), 'public/index.html')).toString();
     response.type('text/html').send(index);
+  }
+
+  @Sse('api/v1/sse/progress')
+  progress(): Observable<MessageEvent> {
+    return this.appService.processLongTask();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { randomUUID } from 'crypto';
+import { interval, map, takeWhile } from 'rxjs';
+
+const MAX_PROGRESS_INTERACTIONS = 100;
+const MAX_INCREMENT = 15;
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  processLongTask() {
+    let progress = 1;
+    let increment = 1;
+    return interval(1000).pipe(
+      map((_) => {
+        increment = Math.floor(Math.random() * MAX_INCREMENT) + 1;
+        progress = Math.min(progress + increment, MAX_PROGRESS_INTERACTIONS);
+        return {
+          data: { progress, description: this.getDescription(progress) },
+          id: randomUUID(),
+        };
+      }),
+      takeWhile((i) => i.data.progress < MAX_PROGRESS_INTERACTIONS, true),
+    );
+  }
+
+  getDescription(progress: number): string {
+    if (progress <= 25) {
+      return 'Requesting authentication ...';
+    } else if (progress <= 50) {
+      return 'Progressing transaction ....';
+    } else if (progress <= 75) {
+      return 'Emitting result for third-party services ...';
+    } else {
+      return 'Summarizing data and collecting metrics ...';
+    }
   }
 }
